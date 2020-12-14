6a4e5ab98e7598deb1ba437a07399f2fb40e55d7
aqazi328 <70555455+aqazi328@users.noreply.github.com>
2020-12-14 00:07:35 -0600

    Update ImageSynthesisNoise.py

--- src/project/ImageSynthesisNoise.py
+++ src/project/ImageSynthesisNoise.py
@@ -1,8 +1,14 @@
 import numpy as np
-
+import cv2
+import signal
+import psychopy
+import scipy
 
 def idealLowpassFilter(emptymask, cutoff):
-    mask = None
+    # mask = None
+    # kernel = np.ones((5, 5), np.float32) / 25
+    # mask = cv2.filter2D(emptymask, -1, kernel)
+    mask = cv2.blur(emptymask, (1,1))
     return mask
 
 
@@ -12,22 +18,32 @@ def idealHighpassFilter(emptymask, cutoff):
 
 
 def gaussianLowpassFilter(emptymask, cutoff):
-    mask = None
+    mask = cv2.GaussianBlur(emptymask, (1,1), cutoff)
     return mask
 
 
 def gaussianHighpassFilter(emptymask, cutoff):
-    mask = None
+    # mask = cv2.GaussianBlur(emptymask, (3,3), cutoff, borderType=cv2.BORDER_CONSTANT)
+    mask = cv2.GaussianBlur(emptymask, (1, 1), cutoff)
     return mask
 
 
 def butterworthLowpassFilter(emptymask, cutoff, order):
-    mask = None
+    # mask = None
+    # mask = psychopy.filters.butter2d_lp(emptymask, cutoff, order)
+    # mask = scipy.signal.butter(emptymask, cutoff, order)
+
+    b, a = signal.butter(order, cutoff, btype='low')
+    mask = signal.filtfilt(b, a, emptymask)
+
     return mask
 
 
 def butterworthHighpassFilter(emptymask, cutoff, order):
-    mask = None
+    # mask = None
+    b, a = signal.butter(order, cutoff, btype='high', analog=False)
+    mask = signal.filtfilt(b, a, emptymask)
+
     return mask
 
 
